149e226e6bab7f5f9c15e0315a31577d5109084e
z5ouyang <zouyang@edge-hpc-log-102.cm.cluster>
2022-08-08 21:17:16 -0400

    add SLURM

--- src/scPipe.py
+++ src/scPipe.py
@@ -85,9 +85,23 @@ def submit_cmd(cmds,config,core=None,memG=0):
       for k,one in failedJobs.items():
         print("\t--->ERROR failed with %d times qsub: %s"%(maxJobSubmitRepN,one))
       print("\n*** Please check log files in %s folder and consider rerun the analysis!"%jID)
-
   elif parallel=="slurm":
-    pass
+    jID = sbatch(cmds,config['output'],core,memG=memG)
+    print("----- Monitoring all submitted SLURM jobs: %s ..."%jID)
+    ## in case of long waiting time to avoid Recursion (too deep)
+    cmdN = {one:1 for one in cmds.keys()}
+    failedJobs = {}
+    while True:
+      squeue(jID,config['output'],cmds,cmdN,core,memG,failedJobs)
+      if len(cmds)==0:
+        break
+      ## wait for 1 min if any running jobs
+      time.sleep(60)
+    if len(failedJobs)>0:
+      print("\n\n*** The following jobs failed:")
+      for k,one in failedJobs.items():
+        print("\t--->ERROR failed with %d times qsub: %s"%(maxJobSubmitRepN,one))
+      print("\n*** Please check log files in %s folder and consider rerun the analysis!"%jID)
   else:
     print("ERROR: unknown parallel setting: %s"%parallel)
     exit()
@@ -173,6 +187,104 @@ def qstat(jID,strPath,cmds,cmdN,core,memG,failedJobs):
   #  ## wait for 1 min if any running jobs
   #  time.sleep(60)
   #  qstat(jID,strPath,cmds,core,iN)
+
+def sbatch(cmds,strPath,core,memG=0,jID=None):
+  if jID is None:
+    jID = "j%d"%random.randint(10,99)
+  strWD = os.path.join(strPath,jID)
+  try:
+    os.makedirs(strWD)
+  except FileExistsError:
+    pass
+  if memG==0:
+    pScript = "\n".join([i for i in sbatchScript.split("\n") if not "MEMFREE" in i])
+  else:
+    pScript = sbatchScript.replace('MEMFREE',str(memG))
+  for one in cmds.keys():
+    oneScript = (pScript.replace('CoreNum',str(core))
+                            .replace('jName',one)
+                            .replace('wkPath',strWD)
+                            .replace("jID",jID)
+                            .replace("sysPath",strPipePath)
+                            .replace("strCMD",cmds[one]))
+    strF=os.path.join(strWD,"%s.sh"%one)
+    with open(strF,"w") as f:
+      f.write(oneScript)
+    run_cmd("sbatch %s"%strF)
+  return jID
+def squeue(jID,strPath,cmds,cmdN,core,memG,failedJobs):
+  print(".",end="")
+  strWD = os.path.join(strPath,jID)
+  qstateCol=4 # make sure the state of qstat is the 4th column (0-index)
+  qJobCol=0 # make sure the job-id of qstat is the 0th column (0-index)
+  # remove the job in wrong stats: Eqw and obtain the running and waiting job names
+  qs = run_cmd("squeue | grep '%s_'"%jID).stdout.decode("utf-8").split("\n")
+  jNames = []
+  for one in qs:
+    if len(one)<5:
+      continue
+    oneJob = [a.strip() for a in one.split(" ") if len(a.strip())>0 ]
+    if oneJob[qstateCol] in ['S','ST']:
+      run_cmd("scancel %s"%oneJob[qJobCol])
+      continue
+    jobDetail = parseSlurmJob(oneJob[qJobCol])
+    jName=jobDetail.get('JobName')
+    if jName is None:
+      continue
+    jNames.append(jName)
+  # check the finished job, resubmit if not sucessfully finished
+  resub = {}
+  finishedJob = []
+  for one in cmds.keys():
+    if not one in jNames:
+      strLog = os.path.join(strWD,"%s.log"%one)
+      if not "DONE" in run_cmd("tail -n 1 %s"%strLog).stdout.decode("utf-8"):
+        cmdN[one] += 1
+        if cmdN[one]>maxJobSubmitRepN:
+          #print("\n--->ERROR failed with %d times qsub: %s"%(maxJobSubmitRepN,one))
+          failedJobs[len(failedJobs)+1] = one
+          finishedJob.append(one)
+          continue
+        resub[one] = cmds[one]
+      else:
+        print("\n\t===== %s ====="%one)
+        with open(strLog,"r") as f:
+          print(f.read())
+        print("\tFinished: %s"%one)
+        finishedJob.append(one)
+  if len(resub)>0:
+    re1=sbatch(resub,strPath,core,memG,jID)
+    time.sleep(5) #might not needed for the qsub to get in
+  for one in finishedJob:
+    del cmds[one]
+def parseSlurmJob(jobID):
+  jInfo = {}
+  for one in (run_cmd("scontrol show job %s"%jobID)
+              .stdout
+              .decode("utf-8")
+              .split("\n")):
+    tmp = one.split("=")
+    if len(tmp)<2:
+      continue
+    if len(tmp)==2:
+      jInfo[tmp[0].strip()]=tmp[1].strip()
+      continue
+    k=tmp[0].strip()
+    v=""
+    for i in range(1,len(tmp)-1):
+      vk=tmp[i].rsplit(" ",1)
+      if len(vk)<2:
+        v += "="+vk[0]
+        continue
+      if len(v)>0:
+        vk[0]= v.strip("=")+"="+vk[0]
+      jInfo[k] = vk[0]
+      k=vk[1].strip()
+    if len(v)>0:
+      tmp[-1] = v.strip("=")+"="+tmp[-1]
+    jInfo[k]=tmp[-1].strip()
+  return(jInfo)
+
 
 ## msg
 def MsgPower():
@@ -855,7 +967,22 @@ set -e
 env -i bash -c 'source sysPath/src/.env;eval $condaEnv;strCMD'
 echo 'DONE'
 '''
-sbatch='''#!/bin/bash
+sbatchScript='''#!/bin/bash
+#SBATCH -J jID_jName
+#SBATCH -D wkPath
+#SBATCH -n node qsubCore
+#SBATCH --mem=MEMFREEG
+#SBATCH -o jName.log
+#SBATCH -e jName.log
+#- End UGE embedded arguments
+echo $SLURM_JOB_NODELIST
+echo 'end of HOST'
+
+# exit
+set -e
+
+env -i bash -c 'source sysPath/src/.env;eval $condaEnv;strCMD'
+echo 'DONE'
 '''
 if __name__ == "__main__":
   start_time = time.time()
