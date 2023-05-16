9131f679b1806b0c996e6d782a3331ae4eeabac4
z5ouyang <z5ouyang@ucsd.edu>
2023-05-16 13:53:47 -0400

    Handle large datasets by batchs for sctHarmony

--- src/sctHarmony.R
+++ src/sctHarmony.R
@@ -44,14 +44,15 @@ processH5ad <- function(strH5ad,batch,strOut,bPrepSCT){
     features <- SelectIntegrationFeatures(Dlist,nfeatures=selFN)
     if(length(Dlist)==1) D <- Dlist[[1]]
     else{
-      #D <- merge(Dlist[[1]], y=Dlist[-1],project=assayName)
+      D <- merge(Dlist[[1]], y=Dlist[-1],project=assayName)
       # according to the test the blow save almost half of memory comparing the above
-      D <- Dlist[[1]]
-      Dlist[[1]] <- NULL
-      for(i in 1:length(Dlist)){
-        D <- merge(D,Dlist[[1]])
-        Dlist[[1]] <- NULL
-      }
+      #D <- Dlist[[1]]
+      #Dlist[[1]] <- NULL
+      #for(i in 1:length(Dlist)){
+      #  D <- merge(D,Dlist[[1]])
+      #  Dlist[[1]] <- NULL
+      #  gc()
+      #}
     }
     message("memory usage after merging: ",sum(sapply(ls(),function(x){object.size(get(x))})),"B for ",cellN," cells")
     rm(Dlist)
@@ -87,15 +88,101 @@ processH5ad <- function(strH5ad,batch,strOut,bPrepSCT){
   data.table::fwrite(X,strOut)
   message("complete in R")
 }
+processSCT <- function(strH5ad,batch,strOut,bPrepSCT){
+  strtemp <- paste0(strOut,".rds")
+  if(file.exists(strtemp)){
+    message("pickup revious results: ",strtemp)
+    X <- readRDS(strtemp)
+  }else{
+    assayName <- "sctHarmony"
+    dimN <- 50
+    X <- getX(strH5ad)
+    gID <- setNames(rownames(X),gsub("_","-",rownames(X)))
+    rownames(X) <- names(gID) #gsub("_","-",rownames(X))
+    D <- CreateSeuratObject(counts=X,
+                            project=assayName,
+                            meta.data=getobs(strH5ad))
+    cellN <- dim(D)[2]
+    rm(X)
+    Dlist <- SplitObject(D,split.by=batch)
+    #message("memory usage before SCT: ",sum(sapply(ls(),function(x){object.size(get(x))})),"B for ",cellN," cells")
+    rm(D)
+    message("\tSCT ...")
+    Dlist <- sapply(Dlist,function(one){
+      bID <- one@meta.data[1,batch]
+      message("\t\t",bID)
+      one <- suppressMessages(suppressWarnings(
+        SCTransform(one,method = 'glmGamPoi',
+                    new.assay.name=assayName,
+                    return.only.var.genes = FALSE,
+                    verbose = FALSE)
+      ))
+      one <- FindVariableFeatures(one, selection.method = "vst", nfeatures = 5000,verbose=F)
+      return(one)
+    })
+    message("\tsaving ...")
+    D <- NULL
+    for(i in 1:length(Dlist)){
+      message("\t\t",Dlist[[1]]@meta.data[1,batch])
+      oneD <- t(Dlist[[1]]@assays[[assayName]]@scale.data[Dlist[[1]]@assays[[assayName]]@var.features,])
+      if(is.null(D)) D <- oneD
+      else{
+        g <- intersect(colnames(D),colnames(oneD))
+        D <- rbind(D[,g],oneD[,g])
+      }
+      Dlist[[1]] <- NULL
+      #print(gc())
+    }
+    rm(Dlist)
+    D <- cbind(cID=rownames(D),data.frame(D))
+    data.table::fwrite(D,strOut)
+  }
+  
+}
+
+processPCA <- function(strPCA,strOut,batch){
+  message("starting Harmony ...")
+  PCA <- getobsm(strPCA,"X_pca")
+  meta <- getobs(strPCA)
+  PCA <- harmony::HarmonyMatrix(PCA,meta,batch,do_pca=FALSE,verbose=FALSE)
+  message("Finishing Harmony ...")
+  dimN <- ncol(PCA)
+  D <- CreateSeuratObject(counts=matrix(0,nrow=2,ncol=nrow(PCA),dimnames=list(paste0("G",1:2),rownames(PCA))),
+                          project="sctHarmony",
+                          meta.data=meta)
+  D[['harmony']] <- CreateDimReducObject(embeddings=PCA,key="pca_",assay="RNA")
+  message("Find Neighbor ...")
+  D <- FindNeighbors(D, dims = 1:dimN,reduction="harmony",verbose = FALSE)
+  message("UMAP ...")
+  D <- RunUMAP(D,reduction="harmony",dims = 1:dimN,verbose = FALSE)
+  message("tSNE ...")
+  D <- RunTSNE(D,reduction="harmony",dims = 1:dimN,verbose = FALSE)
+  message("Clustering ...")
+  D <- FindClusters(D,verbose = FALSE)
+  
+  message("saving ...")
+  X <- cbind(cID=row.names(D@meta.data),
+             D@reductions$harmony@cell.embeddings,
+             D@reductions$umap@cell.embeddings,
+             D@reductions$tsne@cell.embeddings,
+             D@meta.data[,c("seurat_clusters",batch)])
+  colnames(X) <- gsub("UMAP","umap",gsub("harmony","pca",gsub("seurat_clusters","sctHarmony_cluster",colnames(X))))
+  data.table::fwrite(X,strOut)
+}
 
 main <- function(){
   suppressMessages(suppressWarnings(PKGloading()))
   batchKey="library_id" #"batch"
   args = commandArgs(trailingOnly=TRUE)
-  if(length(args)<3) stop("Path to h5ad file, the output file and config file are required!")
+  if(length(args)<2) stop("Path to h5ad file, the output file and config file are required!")
+  source(paste0(dirname(gsub("--file=","",grep("file=",commandArgs(),value=T))),"/readH5ad.R"))
   strH5ad <- args[1]
   if(!file.exists(strH5ad)) stop(paste0("H5ad file (",strH5ad,") does not exist!"))
   strOut <- args[2]
+  if(length(args)<3){
+    print(system.time(processPCA(strH5ad,strOut,batchKey)))
+    return()
+  }
   strConfig <- args[3]
   if(!file.exists(strConfig)){
     stop(paste0("Config file (",strConfig,") does not exist!"))
@@ -103,10 +190,8 @@ main <- function(){
     config <- yaml::read_yaml(strConfig)
   }
   if(length(args)>3) batchKey <- args[4]
-  
-  
-  source(paste0(dirname(gsub("--file=","",grep("file=",commandArgs(),value=T))),"/readH5ad.R"))
-  print(system.time(processH5ad(strH5ad,batchKey,strOut,config$PrepSCTFindMarkers)))
+  #print(system.time(processH5ad(strH5ad,batchKey,strOut,config$PrepSCTFindMarkers)))
+  print(system.time(processSCT(strH5ad,batchKey,strOut,config$PrepSCTFindMarkers)))
 }
 
 main()
--- src/sctHarmony.py
+++ src/sctHarmony.py
@@ -1,43 +1,137 @@
 #!/usr/bin/env python
 
-import subprocess, os, h5py, sys, warnings, re, yaml
+import subprocess, os, h5py, sys, warnings, re, yaml, logging, glob, functools
 from scipy import sparse
 from scipy.sparse import csc_matrix
 import pandas as pd
 import anndata as ad
+import scanpy as sc
 
+print=functools.partial(print, flush=True)
+
+logging.disable()
+strPipePath=os.path.dirname(os.path.realpath(__file__))
 def msgError(msg):
   print(msg)
   exit()
 
-def main():
-  print("starting SCT+Harmony ...")
-  batchKey="library_id"
-  if len(sys.argv)<1:
-    msgError("ERROR: raw h5ad file required!")
-  strH5ad = sys.argv[1]
+def inputCheck(args):
+  strH5ad = args[1]
   if not os.path.isfile(strH5ad):
     msgError("ERROR: %s does not exist!"%strH5ad)
-  #if not strH5ad.endswith("raw.h5ad"):
-  #  msgError("ERROR: %s is not raw h5ad file required!"%strH5ad)
   strConfig = sys.argv[2]
   if not os.path.isfile(strConfig):
     msgError("ERROR: %s does not exist!"%strConfig)
   with open(strConfig,"r") as f:
     config = yaml.safe_load(f)
+  return config
 
-  strCSV = "%s.csv"%os.path.join(config["output"],"sctHarmony",config["prj_name"])#strH5ad.replace("raw.h5ad","sctHarmony.csv")
-  if os.path.isfile(strCSV):
-    print("Using previous harmony results: %s\n***=== Important: If a new run is desired, please remove/rename the above file "%strCSV)
+def splitBatch(strH5ad,strPCA):
+  with open(strPipePath+"/sys.yml","r") as f:
+    sysCon = yaml.safe_load(f)
+  batchCell= sysCon.get("batchCell")
+  if batchCell is None:
+    print("Batch process (batchCell) is not set in sys.yml, large amount of memory might be required")
+    h5adList=[strH5ad]
   else:
-    cmd = "Rscript %s %s %s %s |& tee %s/sctHarmony.log"%(os.path.join(os.path.dirname(os.path.realpath(__file__)),"sctHarmony.R"),
-                              strH5ad,strCSV,strConfig,os.path.dirname(strCSV))
-    subprocess.run(cmd,shell=True,check=True)
-    if not os.path.isfile(strCSV):
-      msgError("\tERROR: sctHarmony failed!")
+    print("Batch process")
+    strOut=os.path.dirname(strPCA)+"/tmp/"
+    os.makedirs(strOut,exist_ok=True)
+    h5adList= glob.glob(strOut+"*.h5ad")
+    if len(h5adList)==0:
+      print("Reading ...")
+      D=ad.read_h5ad(strH5ad)
+      sampleCellN = D.obs.library_id.value_counts()
+      print(sampleCellN)
+      sID=[]
+      cellN=0
+      batchN=0
+      for one in list(sampleCellN.index):
+        sID.append(one)
+        cellN+=sampleCellN[one]
+        print(cellN)
+        if cellN>batchCell or one==list(sampleCellN.index)[-1]:
+          print("batch %d: %d samples"%(batchN,len(sID)))
+          strH5ad=strOut+"tmp_%d.h5ad"%batchN
+          batchN +=1
+          with open(re.sub("h5ad$","txt",strH5ad),"w") as f:
+            f.write("\n".join(sID))
+          with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            D1=D[D.obs.library_id.isin(sID),:]
+            D1.write(strH5ad)
+          sID=[]
+          cellN=0
+          h5adList.append(strH5ad)
+      del D
+  return(h5adList)
 
+def runOneSCT(oneH5ad,strConfig,oneCSV):
+  cmd = "Rscript %s %s %s %s |& tee %s/sctHarmony.log"%(os.path.join(strPipePath,"sctHarmony.R"),
+                              oneH5ad,oneCSV,strConfig,os.path.dirname(oneCSV))
+  subprocess.run(cmd,shell=True,check=True)
+
+def sct(strH5ad,strConfig,strPCA,batchKey):
+  h5adList = splitBatch(strH5ad,strPCA)
+  if len(h5adList)==0:
+    msgError("No h5ad!")
+  print("There are total of %d batches"%len(h5adList))
+  sctD=None
+  for oneH5ad in h5adList:
+    print("***** batch: %s *****"%os.path.basename(oneH5ad))
+    oneCSV = re.sub("h5ad$","csv",oneH5ad)
+    if not os.path.isfile(oneCSV):
+      runOneSCT(oneH5ad,strConfig,oneCSV)
+    if not os.path.isfile(oneCSV):
+      msgError("\tERROR: %s sctHarmony failed in SCT step!"%os.path.basename(oneH5ad))
+    oneD=sc.read_csv(oneCSV)
+    print("***** finishing  *****\n\n\n\n")
+    if sctD is None:
+      sctD = oneD
+    else:
+      sctD = sctD.concatenate(oneD,batch_key=None,index_unique=None)
+  batchV=sc.read_h5ad(strH5ad,backed="r").obs[batchKey].copy()
+  sctD.obs[batchKey]=batchV[sctD.obs.index]
+  sc.tl.pca(sctD, svd_solver='arpack')
+  with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    sctD.write(strPCA)
+  return None
+
+def runRharmony(strPCA,strCSV):
+  cmd = "Rscript %s %s %s |& tee %s/sctHarmony.log"%(os.path.join(strPipePath,"sctHarmony.R"),
+                              strPCA,strCSV,os.path.dirname(strCSV))
+  subprocess.run(cmd,shell=True,check=True)
+
+def sctHarmony(strH5ad,strConfig,strCSV,batchKey):
+  if os.path.isfile(strCSV):
+    print("Using previous sctHarmony results: %s\n***=== Important: If a new run is desired, please remove/rename the above file "%strCSV)
+    meta = pd.read_csv(strCSV,index_col=0,header=0)
+    meta.index = list(meta.index)
+    return(meta)
+  strPCA = re.sub("csv$","pca.h5ad",strCSV)
+  sct(strH5ad,strConfig,strPCA,batchKey)
+  runRharmony(strPCA,strCSV)
+  if not os.path.isfile(strCSV):
+    msgError("\tERROR: %s sctHarmony failed in final harmony step!")
   meta = pd.read_csv(strCSV,index_col=0,header=0)
   meta.index = list(meta.index)
+  meta.to_csv(strCSV)
+  print("sctHarmony completed")
+  return(meta)
+
+def main():
+  print("starting SCT+Harmony ...")
+  batchKey="library_id"
+  if len(sys.argv)<2:
+    msgError("ERROR: raw h5ad file and the config file are required!")
+  config = inputCheck(sys.argv)
+  strH5ad = sys.argv[1]
+  strConfig=sys.argv[2]
+
+  strCSV = "%s.csv"%os.path.join(config["output"],"sctHarmony",config["prj_name"])#strH5ad.replace("raw.h5ad","sctHarmony.csv")
+  meta = sctHarmony(strH5ad,strConfig,strCSV,batchKey)
+
   for one in meta.columns:
     if meta[one].nunique()<100:
       meta[one]=meta[one].astype("category")
