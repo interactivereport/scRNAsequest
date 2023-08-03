b8b575d969d949ee8bca3f06f797956c5d321e92
z5ouyang <z5ouyang@ucsd.edu>
2023-08-03 17:23:20 -0400

    fix bug of reference name for major cluster label

--- src/mergeH5ad.py
+++ src/mergeH5ad.py
@@ -19,13 +19,23 @@ rawSuffix = "_raw_obsAdd.h5ad"
 def msgError(msg):
   print("***** "+msg)
   exit()
+def getRefName(config):
+  # can be dict, name of a reference, or a path to a rds file
+  refName = config.get("ref_name") # can be dict, name of a reference, or a rds
+  if refName is None:
+    refName='None'
+  elif isinstance(refName,dict):
+    refName=",".join(refName.keys())
+  else:
+    refName=','
+  return refName
 def combine(mIDs,prefix,config):
   CKmethods = [one for one in mIDs if os.path.isfile("%s.h5ad"%os.path.join(config['output'],one,config["prj_name"]))]+['raw']
   if not "SCT" in CKmethods:
     msgError("SCT is missing! and it is required expression for visualization!")
   
   clusterR = 2 if config.get("major_cluster_rate") is None else config.get("major_cluster_rate")
-  refName = str(config.get("ref_name"))
+  refName = getRefName(config)
   print("combining all methods results ...")
   strH5ad = prefix+".h5ad"
   strRaw = prefix+rawSuffix
@@ -82,7 +92,7 @@ def obsCOR(obs,strH5ad):
   strPDF = os.path.join(os.path.dirname(strH5ad),"obsCor.pdf")
   with PdfPages(strPDF) as pdf:
     for a in obs.columns:
-      print(a,"\t",list(obs.columns).index(a),"/",obs.shape[1])
+      print("\t%d/%d: %s"%(list(obs.columns).index(a),obs.shape[1],a))
       if obs[a].nunique()<2:
         continue
       obsT = obs.copy()
@@ -128,7 +138,6 @@ def obsCOR(obs,strH5ad):
       gc.collect()
   pVal.to_csv(re.sub('pdf$','csv',strPDF))
 
-
 def integrateH5ad(strH5ad,methods,prefix,outH5ad,majorR=None,ref_name=None):
   print("reading h5ad for integration")
   D = ad.read_h5ad(strH5ad)
@@ -148,9 +157,7 @@ def integrateH5ad(strH5ad,methods,prefix,outH5ad,majorR=None,ref_name=None):
     addObs = [one for one in obs.columns if not one in D.obs.columns]
     if one == "SeuratRef":
       seuratRefCluster=obs.columns
-      prefix_ref=""
-      if isinstance(ref_name,dict):
-        prefix_ref=list(ref_name.keys())[0]
+      prefix_ref="" if ref_name is None else ref_name.split(',')[0]
       seuratRefLab = [i for i in addObs if not i.endswith("score") and i.startswith("predicted."+prefix_ref)]
     if len(addObs)>0:
       D.obs=D.obs.merge(obs[addObs],how="left",left_index=True,right_index=True)
@@ -204,7 +211,6 @@ def main():
   prefix=sys.argv[4]
   clusterR=float(sys.argv[5])
   refName=None if sys.argv[6]=="None" else sys.argv[6]
-  print(refName)
   integrateH5ad(inH5ad,methods,prefix,outH5ad,clusterR,refName)
 
 if __name__ == "__main__":
