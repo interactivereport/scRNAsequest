6bda9280d3b37cc505bd8b366c271acc01bfed12
z5ouyang <z5ouyang@ucsd.edu>
2024-04-18 22:00:49 -0400

    update RPCA with seurat5

--- src/seuratRPCA.R
+++ src/seuratRPCA.R
@@ -1,19 +1,52 @@
 
 PKGloading <- function(){
   require(Seurat)
-  require(RcppCNPy)
-  require(readr)
+  require(peakRAM)
+  #require(RcppCNPy)
+  #require(readr)
   options(stringsAsFactors = FALSE)
-  if(F){
-    rlang::env_unlock(env = asNamespace('base'))
-    rlang::env_binding_unlock(env = asNamespace('base'))
-    message <<- function(...,domain = NULL, appendLF = TRUE){
-      cat(paste(...,collapse=", "),"\n",sep="")
-    }
-    rlang::env_binding_lock(env = asNamespace('base'))
-    rlang::env_lock(asNamespace('base'))
-  }
+}
+
+runRPCA <- function(strH5ad,batch,strOut,cluster_method,clusterResolution){
+  D <- CreateSeuratObject(counts=getX(strH5ad),
+                          project="RPCA",
+                          meta.data=getobs(strH5ad))
   
+  D[["RNA"]] <- split(D[["RNA"]],f=unlist(D[[batch]],use.names=F))
+  message("\tSCT ...")
+  D <- SCTransform(D,vst.flavor="v2",
+                   return.only.var.genes=F,
+                   verbose=F)
+  if(length(D[["RNA"]]@layers)>1){
+    D <- RunPCA(D,npcs=50,verbose = FALSE)
+    D <- IntegrateLayers(D,method=RPCAIntegration,
+                         orig.reduction = "pca",
+                         normalization.method="SCT",
+                         verbose = F)
+  }
+  D <- RunPCA(D,npcs=50,verbose = FALSE)
+  D <- RunUMAP(D,dims=1:50,
+               reduction="pca",
+               umap.method="umap-learn",
+               metric = "correlation",
+               verbose=F)
+  D <- FindNeighbors(D,dims = 1:50,verbose=F)
+  
+  message("Clustering ",cluster_method," (",clusterResolution,") ...")
+  if(grepl("Leiden",cluster_method,ignore.case=T)){
+    D <- FindClusters(D,verbose = FALSE,resolution=clusterResolution,method="igraph",algorithm=4)
+  }else{
+    D <- FindClusters(D,verbose = FALSE,resolution=clusterResolution)#default Algorithm: 1 = original Louvain algorithm
+  }
+  # prepare saving
+  X <- cbind(D[["seurat_clusters"]],
+             D[["pca"]]@cell.embeddings,
+             D[["umap"]]@cell.embeddings)
+  colnames(X) <- gsub("^PC","pca",colnames(X))
+  colnames(X) <- gsub("^UMAP","umap",colnames(X))
+  colnames(X) <- gsub("seurat_clusters","seuratRPCA_cluster",colnames(X))
+  X <- cbind(cID=rownames(X),X)
+  write_csv(X,strOut)
 }
 
 main <- function(){
@@ -25,66 +58,8 @@ main <- function(){
   clusterResolution <- as.numeric(args[3])
   cluster_method <- args[4]
   source(paste0(dirname(gsub("--file=","",grep("file=",commandArgs(),value=T))),"/readH5ad.R"))
-  print(system.time({
-    D <- CreateSeuratObject(counts=getX(strH5ad),
-                                 project="SCT",
-                                 meta.data=getobs(strH5ad))
-    # split cells based on project id
-    Dlist <- SplitObject(D, split.by = batchKey)
-    ## following https://satijalab.org/seurat/articles/integration_rpca.html (with SCTransform)
-    #Dmedian <- median(colSums(D@assays$RNA@counts))
-    rm(D)
-    gc()
-    Dlist <- sapply(Dlist,function(one,medianUMI){
-      message("\t\tSCT one ...")
-      #after checking/testing, the above would return proper SCT nomralized data
-      #/edgehpc/dept/compbio/users/zouyang/process/PRJNA544731/src/SCT_scale_batch.ipynb
-      #https://github.com/satijalab/sctransform/issues/128
-      return(suppressMessages(suppressWarnings(
-        SCTransform(one,method = 'glmGamPoi',
-                    new.assay.name="SCT",
-                    return.only.var.genes = FALSE,
-                    #scale_factor=medianUMI,
-                    verbose = FALSE)
-      )))
-    },Dmedian)
-    if(length(Dlist)>1){
-      features <- SelectIntegrationFeatures(object.list = Dlist, nfeatures = 3000)
-      Dlist <- PrepSCTIntegration(object.list = Dlist, anchor.features = features)
-      Dlist <- lapply(Dlist, FUN = RunPCA, features = features)
-      
-      anchors <- FindIntegrationAnchors(Dlist, normalization.method = "SCT",
-                                        anchor.features = features, dims = 1:50,
-                                        reduction = "rpca", k.anchor = 20)
-      rm(Dlist)
-      gc()
-      Dsct <- IntegrateData(anchorset = anchors, normalization.method = "SCT", dims = 1:50)
-      rm(anchors)
-      gc()
-    }else{
-      Dsct <- Dlist[[1]]
-    }
-    
-    Dsct <- RunPCA(Dsct, verbose = FALSE)
-    Dsct <- RunUMAP(Dsct, reduction = "pca", dims = 1:50)
-    Dsct <- FindNeighbors(Dsct,dims = 1:50)
-    
-    message("Clustering ",cluster_method," (",clusterResolution,") ...")
-    if(grepl("Leiden",cluster_method,ignore.case=T)){
-      Dsct <- FindClusters(Dsct,verbose = FALSE,resolution=clusterResolution,algprithem=4)
-    }else{
-      Dsct <- FindClusters(Dsct,verbose = FALSE,resolution=clusterResolution)#default Algorithm: 1 = original Louvain algorithm
-    }
-    # prepare saving
-    D <- cbind(Dsct[["seurat_clusters"]],
-               Dsct[["pca"]]@cell.embeddings,
-               Dsct[["umap"]]@cell.embeddings)
-    colnames(D) <- gsub("^PC","pca",colnames(D))
-    colnames(D) <- gsub("^UMAP","umap",colnames(D))
-    colnames(D) <- gsub("seurat_clusters","seuratRPCA_cluster",colnames(D))
-    D <- cbind(cID=rownames(D),D)
-    write_csv(D,args[2])
-  }))
+  print(peakRam(
+    runRPCA(strH5ad,batchKey,args[2],cluster_method,clusterResolution)))
 }
 
 main()
