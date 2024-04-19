8c7e0bbafb8506eba2078102359ad7227cad32b5
z5ouyang <z5ouyang@ucsd.edu>
2024-04-18 22:01:16 -0400

    RETICULATE_PYTHON

--- install.sh
+++ install.sh
@@ -40,10 +40,11 @@ echo "export SGE_EXECD_PORT=$SGE_EXECD_PORT" >> $src/.env
 echo "export SGE_QMASTER_PORT=$SGE_QMASTER_PORT" >> $src/.env
 echo "export SGE_ROOT=$SGE_ROOT" >> $src/.env
 echo "export SLURM_CONF=$SLURM_CONF" >> $src/.env
+echo "export RETICULATE_PYTHON=$(which python)"
 conda deactivate
 
 ## additional packages which are not available on anaconda
-env -i src="$src" bash -c 'source $src/..env;eval $condaEnv;$src/../install/install.extra'
+env -i src="$src" bash -c 'source $src/.env;eval $condaEnv;$src/../install/install.extra'
 
 echo ""
 echo "If no errors above, scRNAsequest installation is successful!"
